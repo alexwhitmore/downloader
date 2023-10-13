5c617f8afce99dc7f9245298bd3a5542af237426
alexwhitmore <heyimalexw@gmail.com>
2023-10-13 10:17:49 -0600

    More SEO stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
 import '@/styles/globals.css'
+import type { Metadata } from 'next'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Analytics } from '@vercel/analytics/react'
 
 interface RootLayoutProps {
   children: React.ReactNode
+}
+
+export const metadata: Metadata = {
+  title: 'YouTube Video Downloader | Free Download Tool',
+  description:
+    'YouTube Video Downloader is a free and fast tool for downloading unlimited YouTube videos. Easily save videos for offline viewing. Try it now!',
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
