60064c8b85bf416b4c268987d21ff8f349444c70
alexwhitmore <heyimalexw@gmail.com>
2023-10-13 11:34:50 -0600

    Switch Az storage with Supabase

--- index.py
+++ index.py
@@ -4,16 +4,18 @@ from flask_cors import CORS, cross_origin
 from azure.storage.blob import BlobServiceClient
 from yt_dlp import YoutubeDL
 import logging
+from supabase import create_client
 
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 app.config["CORS_HEADERS"] = "Content-Type"
 
-blob_service_client = BlobServiceClient.from_connection_string(
-    "DefaultEndpointsProtocol=https;AccountName=dlprodwus2st;AccountKey=jSoIuWP0h5sFiCOUBDbWmiHr6SfJkKKQBTSKzS/AbMA6qLffbDuB4n1VeKc9KdoYwcjWGOGsOD5t+AStYPtriA==;EndpointSuffix=core.windows.net"
-)
-container_name = "videos"
+SUPABASE_URL = "https://hhltmpiizvmmcjlkxeos.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhobHRtcGlpenZtbWNqbGt4ZW9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjI5MDA3NTEsImV4cCI6MTk3ODQ3Njc1MX0.dwT1L_WD0CZV1XSrc91wqByVOtlgfwWeLm7Dls4aQxk"
+SUPABASE_BUCKET_NAME = "images"
+
+supabase_client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 
 @app.route("/api/upload_youtube", methods=["POST"])
@@ -34,10 +36,16 @@ def upload_youtube_video():
 
         video_file_path = f"/tmp/temp.{info_dict['ext']}"
 
-        # Upload the video file to Azure Blob Storage
-        container_client = blob_service_client.get_container_client(container_name)
+        # Upload the video file to Supabase Storage
         with open(video_file_path, "rb") as video_file:
-            container_client.upload_blob(name="temp.mp4", data=video_file)
+            supabase_client.storage.from_(SUPABASE_BUCKET_NAME).upload(
+                file=video_file,
+                path=f"temp.{info_dict['ext']}",
+                file_options={"content-type": "video/mp4"},
+            )
+
+        # Clean up the temporary video file
+        os.remove(video_file_path)
 
         return jsonify({"message": "Video uploaded successfully."}), 200
     except Exception as e:
@@ -76,22 +84,35 @@ def get_thumbnail_url():
 def download_video():
     try:
         # Specify the file name you want to download
-        blob_name = "temp.mp4"  # Replace with the desired file name
+        file_name = "temp.mp4"  # Replace with the desired file name
 
-        container_client = blob_service_client.get_container_client(container_name)
+        video_file_path = "temp.mp4"
 
-        # Get the blob client for the specified file
-        blob_client = container_client.get_blob_client(blob=blob_name)
+        # Download the video file from Supabase Storage
+        # response = supabase_client.storage.from_(SUPABASE_BUCKET_NAME).download(
+        #     file_name
+        # )
+        with open(video_file_path, "wb+") as f:
+            res = supabase_client.storage.from_(SUPABASE_BUCKET_NAME).download(
+                video_file_path
+            )
+            f.write(res)
 
-        # Check if the file exists in Azure Blob Storage
-        if not blob_client.exists():
-            return jsonify({"error": "File not found."}), 404
+        # Check if the file exists in Supabase Storage
+        # if response.status_code == 404:
+        #     return jsonify({"error": "File not found."}), 404
+        # if response.status_code != 200:
+        #     return jsonify({"error": "Error downloading the file."}), 500
 
-        # Stream the file from Azure Blob Storage to the client
-        response = Response(blob_client.download_blob().readall())
-        response.headers["Content-Disposition"] = f"attachment; filename={blob_name}"
-        return response
+        # # Stream the file to the client as an attachment
+        # file_data = response.content
+        # response = Response(file_data)
+        # response.headers["Content-Disposition"] = f"attachment; filename={file_name}"
+        # return response
+        return jsonify({"message": "Video uploaded successfully."}), 200
     except Exception as e:
+        # Log the error for debugging
+        logging.error(f"Error downloading the file: {str(e)}")
         return jsonify({"error": f"Error downloading the file: {str(e)}"}), 500
 
 
