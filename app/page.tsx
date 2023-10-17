e430801669972f57b2ee95b39adeb07d97748809
alexwhitmore <heyimalexw@gmail.com>
2023-10-17 12:41:25 -0600

    Hide alert on scroll

--- app/page.tsx
+++ app/page.tsx
@@ -9,27 +9,26 @@ import { Footer } from '@/components/footer'
 
 export default function Home() {
   return (
-    <main>
-      <div className="fixed top-0 right-0 mt-4 mr-4 z-50 w-96 hidden md:block">
-        <Alert className="bg-yellow-500/20 border-yellow-500/30">
+    <div className="relative">
+      <div className="absolute top-0 right-0 mt-4 mr-4 z-10">
+        <Alert className="bg-yellow-500/20 border-yellow-500/30 hidden md:block w-96">
           <AlertTriangle className="h-4 w-4 stroke-yellow-500" />
           <AlertTitle className="text-yellow-100">Use with caution</AlertTitle>
-          <AlertDescription className="text-yellow-100/90">
+          <AlertDescription className="text-yellow-100">
             This is a new application so it's possible that there are some bugs.
-            Feel free to reach out{' '}
-            <Button asChild variant="link" className="p-0 h-0 text-yellow-500">
-              <a href="#">here</a>
-            </Button>{' '}
-            if you have any issues or feature requests!
+            Feel free to reach out <a href="#">here</a> if you have any issues
+            or feature requests!
           </AlertDescription>
         </Alert>
       </div>
 
-      <Hero />
-      <Features />
-      <Instructions />
-      <FAQs />
-      <Footer />
-    </main>
+      <main>
+        <Hero />
+        <Features />
+        <Instructions />
+        <FAQs />
+        <Footer />
+      </main>
+    </div>
   )
 }
