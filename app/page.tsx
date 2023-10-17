df99deef0efc06b2b92258ed69b26c749bb968f2
alexwhitmore <heyimalexw@gmail.com>
2023-10-17 15:28:46 -0600

    clean up alert, add to mobile version

--- app/page.tsx
+++ app/page.tsx
@@ -9,15 +9,18 @@ import { Footer } from '@/components/footer'
 
 export default function Home() {
   return (
-    <div className="relative">
-      <div className="absolute top-0 right-0 mt-4 mr-4 z-10">
-        <Alert className="bg-yellow-500/20 border-yellow-500/30 hidden md:block w-96">
+    <div className="md:relative">
+      <div className="flex justify-end md:absolute top-0 right-0 mt-4 px-4 z-10">
+        <Alert className=" bg-yellow-500/20 border-yellow-500/30 sm:w-96">
           <AlertTriangle className="h-4 w-4 stroke-yellow-500" />
-          <AlertTitle className="text-yellow-100">Use with caution</AlertTitle>
+          <AlertTitle className="text-yellow-100">Attention</AlertTitle>
           <AlertDescription className="text-yellow-100">
-            This is a new application so it's possible that there are some bugs.
-            Feel free to reach out <a href="#">here</a> if you have any issues
-            or feature requests!
+            This is a new site and works best on desktop. It's also possible
+            that there are some bugs. Feel free to reach out{' '}
+            <Button asChild variant="link" className="text-yellow-500 p-0 h-0">
+              <a href="https://twitter.com/theAlexWhitmore">here</a>
+            </Button>{' '}
+            if you have any issues or feature requests!
           </AlertDescription>
         </Alert>
       </div>
